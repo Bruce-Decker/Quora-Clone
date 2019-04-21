b435e919c3b3312d138b68981ec89ee337d48539
prachichouksey <prachi.chouksey@sjsu.edu>
2019-04-21 16:39:11 -0700

    updated user questions

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -2,10 +2,30 @@ const express = require("express");
 const router = express.Router();
 const kafka = require("../kafka/client");
 
-router.get("/", function(req, res) {
+router.get("/keyword", function(req, res) {
   kafka.make_request(
     "question",
     { method: "searchQuestion", message: req.query },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ msg: "cannot find questions" });
+      } else {
+        if (result.errors) {
+          return res.status(400).json(result.errors);
+        } else {
+          console.log(result);
+          res.send(result);
+        }
+      }
+    }
+  );
+});
+
+router.get("/user", function(req, res) {
+  kafka.make_request(
+    "question",
+    { method: "userQuestion", message: req.query },
     function(error, result) {
       if (error) {
         console.log(error);
