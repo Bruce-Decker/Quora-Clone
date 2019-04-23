cca235a5b69bb274d68ea28f7e2ca88abeca71ee
prachichouksey <prachi.chouksey@sjsu.edu>
2019-04-22 18:55:28 -0700

    get all topics

--- backend/routes/topic.js
+++ backend/routes/topic.js
@@ -3,21 +3,44 @@ const router = express.Router();
 const kafka = require("../kafka/client");
 
 router.post("/createTopics", function(req, res) {
-  kafka.make_request('topic', {"method": "postTopic", "message": req.body}, function(error, result) {
-    if (error) {
-        console.log(error)
-        res.status(400).json({msg: 'profile not found'});
-    } else {
-        res.send(result)
+  kafka.make_request(
+    "topic",
+    { method: "postTopic", message: req.body },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ msg: "profile not found" });
+      } else {
+        res.send(result);
+      }
     }
-})
-})
-
+  );
+});
 
 router.get("/", function(req, res) {
   kafka.make_request(
     "topic",
     { method: "searchTopic", message: req.query },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ msg: "cannot find topics" });
+      } else {
+        if (result.errors) {
+          return res.status(400).json(result.errors);
+        } else {
+          console.log(result);
+          res.send(result);
+        }
+      }
+    }
+  );
+});
+
+router.get("/all", function(req, res) {
+  kafka.make_request(
+    "topic",
+    { method: "getTopic", message: req.query },
     function(error, result) {
       if (error) {
         console.log(error);
--- kafka-backend/services/topic.js
+++ kafka-backend/services/topic.js
@@ -5,46 +5,65 @@ exports.topicService = function topicService(info, callback) {
     case "searchTopic":
       searchTopic(info, callback);
       break;
+    case "getTopic":
+      searchTopic(info, callback);
+      break;
     case "postTopic":
       postTopic(info, callback);
       break;
   }
 };
 
-
 function postTopic(info, callback) {
-     var topic_id = info.message.topic_id
-     var topic_name = info.message.topic_name
-     var data = {
-       topic_id,
-       topic_name
-     }
+  var topic_id = info.message.topic_id;
+  var topic_name = info.message.topic_name;
+  var data = {
+    topic_id,
+    topic_name
+  };
 
-     Topic.findOne({topic_id: topic_id}, function(err, docs) {
-      if (docs) {
-        Topic.findOneAndUpdate({topic_id: topic_id}, data, function(err, result) {
-           if (err) {
-                 res.send("Fail")
-           } else {
-                 console.log(result)
-                 callback(null, {msg: "Update successfully", data: data})
-            }
-        })
-      } else {    
-        Topic.create(data, function(err, newlyCreated) {
-            if (err) {
-                 callback(null, {msg: "False"});
-            } else {
-                 callback(null, {msg: "True", data: data});
-            }
-          })
-      }
-    })   
+  Topic.findOne({ topic_id: topic_id }, function(err, docs) {
+    if (docs) {
+      Topic.findOneAndUpdate({ topic_id: topic_id }, data, function(
+        err,
+        result
+      ) {
+        if (err) {
+          res.send("Fail");
+        } else {
+          console.log(result);
+          callback(null, { msg: "Update successfully", data: data });
+        }
+      });
+    } else {
+      Topic.create(data, function(err, newlyCreated) {
+        if (err) {
+          callback(null, { msg: "False" });
+        } else {
+          callback(null, { msg: "True", data: data });
+        }
+      });
+    }
+  });
 }
 
 function searchTopic(info, callback) {
   var topic = info.message.topic_name;
   Topic.find({ topic_name: new RegExp(topic, "i") }, function(err, docs) {
+    console.log(docs);
+    console.log(err);
+    if (docs) {
+      console.log(docs);
+      callback(null, docs);
+    } else {
+      console.log(err);
+      callback(err, "error");
+    }
+  });
+}
+
+function getTopic(info, callback) {
+  Topic.find({}, function(err, docs) {
     console.log(docs);
     console.log(err);
     if (docs) {
