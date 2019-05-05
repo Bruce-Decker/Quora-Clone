2a53eeb53e44cd34335e77768b7a7c710340ca75
Pedroska777 <jabbarpour@gmail.com>
2019-05-04 20:39:14 -0700

    profile activate

--- backend/routes/userProfile.js
+++ backend/routes/userProfile.js
@@ -71,12 +71,56 @@ router.post("/deleteProfile", function(req, res) {
         console.log(error);
         res.status(400).json({ msg: "cannot delete user" });
       } else {
-        if (result.errors) {
-          return res.status(400).json(result.errors);
-        } else {
-          console.log(result);
-          res.send(result);
-        }
+        console.log(result);
+        res.send(result);
+      }
+    }
+  );
+});
+
+router.post("/activate", function(req, res) {
+  kafka.make_request(
+    "profile",
+    { method: "activate", message: req.body },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ msg: "cannot activate user" });
+      } else {
+        console.log("result activate", result);
+        res.send(result);
+      }
+    }
+  );
+});
+
+router.post("/deactivate", function(req, res) {
+  kafka.make_request(
+    "profile",
+    { method: "activate", message: req.body },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ msg: "cannot deactivate user" });
+      } else {
+        console.log("result deactivate", result);
+        res.send(result);
+      }
+    }
+  );
+});
+
+router.get("/isActive", function(req, res) {
+  kafka.make_request(
+    "profile",
+    { method: "isActive", message: req.query },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+        res.status(400).json({ msg: "cannot deactivate user" });
+      } else {
+        console.log("result deactivate", result);
+        res.send(result);
       }
     }
   );
