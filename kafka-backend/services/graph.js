748ca732cd26989754fdf8f128856532a15b97ff
prachichouksey <prachi.chouksey@sjsu.edu>
2019-05-06 13:53:38 -0700

    graph changes

--- kafka-backend/services/graph.js
+++ kafka-backend/services/graph.js
@@ -1,104 +1,149 @@
 var Question = require("../models/Question");
-
 exports.graphService = function graphService(info, callback) {
-    switch (info.method) {
-      case "upvote":
-        upvoteGraph(info, callback);
-        break;
-        case "downvote":
-        downvoteGraph(info, callback);
-        break;
-        case "bookmark":
-        bookmarkGraph(info, callback);
-        break;
-    }
-}
+  switch (info.method) {
+    case "upvote":
+      upvoteGraph(info, callback);
+      break;
+    case "downvote":
+      downvoteGraph(info, callback);
+      break;
+    case "bookmark":
+      bookmarkGraph(info, callback);
+      break;
+  }
+};
 
-function upvoteGraph(info,callback)
-{
-  console.log(info.message)
-  Question.find(
-    {},
-    (err, questions) => {
-      if (err) {
-        callback(err, null);
-      } else {  
-        var answerUpvoteMap = [];
-          for(let i =0 ; i < questions.length; i++){
-              if(questions[i].answers && questions[i].answers.length>0){
-                let currentAnswersList = questions[i].answers;
-                for(let j =0 ; j<currentAnswersList.length; j++ ){
-                    let answer_id = currentAnswersList[j].answer_id;
-                    answerUpvoteMap.push({"answer_id":answer_id, "upvoteCount":currentAnswersList[j].upvote.length,"answerContent":currentAnswersList[j].answerContent});
-                }
-              }
+function upvoteGraph(info, callback) {
+  console.log(info.message);
+  Question.find({}, (err, questions) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var answerUpvoteMap = [];
+      for (let i = 0; i < questions.length; i++) {
+        if (questions[i].answers && questions[i].answers.length > 0) {
+          let currentAnswersList = questions[i].answers;
+          for (let j = 0; j < currentAnswersList.length; j++) {
+            //let answer_id = currentAnswersList[j].answer_id;
+            let answerContent = currentAnswersList[j].answerContent;
+            if (answerContent != null) {
+              answerContent = answerContent.replace(/<\/?.+?>/gi, "");
+              //answerUpvoteMap.push({"answer_id":answer_id, "upvoteCount":currentAnswersList[j].upvote.length,"answerContent":currentAnswersList[j].answerContent});
+              answerUpvoteMap.push({
+                answerContent: answerContent,
+                upvoteCount: currentAnswersList[j].upvote.length
+              });
+            } else {
+              let answer_id = currentAnswersList[j].answer_id;
+              answerUpvoteMap.push({
+                answer_id: answer_id,
+                upvoteCount: currentAnswersList[j].upvote.length
+              });
+            }
           }
-          answerUpvoteMap.sort((a,b) => (a.upvoteCount < b.upvoteCount) ? 1 : ((b.upvoteCount < a.upvoteCount) ? -1 : 0));
-          
-          console.log(answerUpvoteMap);
-          
-        callback(null, answerUpvoteMap);
+        }
       }
+
+      answerUpvoteMap.sort((a, b) =>
+        a.upvoteCount < b.upvoteCount
+          ? 1
+          : b.upvoteCount < a.upvoteCount
+          ? -1
+          : 0
+      );
+      let topUpvote = answerUpvoteMap.slice(0, 10);
+      console.log(topUpvote);
+
+      callback(null, topUpvote);
     }
-  );
+  });
 }
 
-function downvoteGraph(info,callback)
-{
-  console.log(info.message)
-  Question.find(
-    {},
-    (err, questions) => {
-      if (err) {
-        callback(err, null);
-      } else {  
-        var answerDownvoteMap = [];
-          for(let i =0 ; i < questions.length; i++){
-              if(questions[i].answers && questions[i].answers.length>0){
-                let currentAnswersList = questions[i].answers;
-                for(let j =0 ; j<currentAnswersList.length; j++ ){
-                    let answer_id = currentAnswersList[j].answer_id;
-                    answerDownvoteMap.push({"answer_id":answer_id, "downvoteCount":currentAnswersList[j].downvote.length,"answerContent":currentAnswersList[j].answerContent});
-                }
-              }
+function downvoteGraph(info, callback) {
+  console.log(info.message);
+  Question.find({}, (err, questions) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var answerDownvoteMap = [];
+      for (let i = 0; i < questions.length; i++) {
+        if (questions[i].answers && questions[i].answers.length > 0) {
+          let currentAnswersList = questions[i].answers;
+          for (let j = 0; j < currentAnswersList.length; j++) {
+            //let answer_id = currentAnswersList[j].answer_id;
+            let answerContent = currentAnswersList[j].answerContent;
+            if (answerContent != null) {
+              answerContent = answerContent.replace(/<\/?.+?>/gi, "");
+              answerDownvoteMap.push({
+                answerContent: answerContent,
+                downvoteCount: currentAnswersList[j].downvote.length
+              });
+            } else {
+              let answer_id = currentAnswersList[j].answer_id;
+              answerDownvoteMap.push({
+                answer_id: answer_id,
+                downvoteCount: currentAnswersList[j].downvote.length
+              });
+            }
           }
-          answerDownvoteMap.sort((a,b) => (a.downvoteCount < b.downvoteCount) ? 1 : ((b.downvoteCount < a.downvoteCount) ? -1 : 0));
-          
-          console.log(answerDownvoteMap);
-          
-        callback(null, answerDownvoteMap);
+        }
       }
+      answerDownvoteMap.sort((a, b) =>
+        a.downvoteCount < b.downvoteCount
+          ? 1
+          : b.downvoteCount < a.downvoteCount
+          ? -1
+          : 0
+      );
+      let topDownvote = answerDownvoteMap.slice(0, 10);
+
+      console.log(topDownvote);
+
+      callback(null, topDownvote);
     }
-  );
+  });
 }
 
-
-function bookmarkGraph(info,callback)
-{
-  console.log(info.message)
-  Question.find(
-    {},
-    (err, questions) => {
-      if (err) {
-        callback(err, null);
-      } else {  
-        var answerBookmarkMap = [];
-          for(let i =0 ; i < questions.length; i++){
-              if(questions[i].answers && questions[i].answers.length>0){
-                let currentAnswersList = questions[i].answers;
-                for(let j =0 ; j<currentAnswersList.length; j++ ){
-                    let answer_id = currentAnswersList[j].answer_id;
-                    answerBookmarkMap.push({"answer_id":answer_id, "bookmarkCount":currentAnswersList[j].bookmark.length,"answerContent":currentAnswersList[j].answerContent});
-                }
-              }
+function bookmarkGraph(info, callback) {
+  console.log(info.message);
+  Question.find({}, (err, questions) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var answerBookmarkMap = [];
+      for (let i = 0; i < questions.length; i++) {
+        if (questions[i].answers && questions[i].answers.length > 0) {
+          let currentAnswersList = questions[i].answers;
+          for (let j = 0; j < currentAnswersList.length; j++) {
+            let answerContent = currentAnswersList[j].answerContent;
+            if (answerContent != null) {
+              answerContent = answerContent.replace(/<\/?.+?>/gi, "");
+              answerBookmarkMap.push({
+                answerContent: answerContent,
+                bookmarkCount: currentAnswersList[j].bookmark.length
+              });
+            } else {
+              let answer_id = currentAnswersList[j].answer_id;
+              answerBookmarkMap.push({
+                answer_id: answer_id,
+                bookmarkCount: currentAnswersList[j].bookmark.length
+              });
+            }
           }
-          answerBookmarkMap.sort((a,b) => (a.bookmarkCount < b.bookmarkCount) ? 1 : ((b.bookmarkCount < a.bookmarkCount) ? -1 : 0));
-          
-          console.log(answerBookmarkMap);
-          
-        callback(null, answerBookmarkMap);
+        }
       }
-    }
-  );
-}
+      answerBookmarkMap.sort((a, b) =>
+        a.bookmarkCount < b.bookmarkCount
+          ? 1
+          : b.bookmarkCount < a.bookmarkCount
+          ? -1
+          : 0
+      );
+      let topBookmarks = answerBookmarkMap.slice(0, 10);
 
+      console.log(topBookmarks);
+
+      callback(null, topBookmarks);
+    }
+  });
+}
